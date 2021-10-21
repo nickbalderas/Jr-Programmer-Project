39da2ed7730d0698cfbace4a65b8deb47db627c5
Nicholas Balderas <geist732@gmail.com>
2021-10-21 07:05:17 -0500

    Data persistence between sessions

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using UnityEngine;
 
 public class MainManager : MonoBehaviour
@@ -14,5 +16,35 @@ public class MainManager : MonoBehaviour
         }
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadColor();
     }
+    
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+        
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadColor()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            TeamColor = data.TeamColor;
+        }
+    }
+}
+
+[Serializable]
+class SaveData
+{
+    public Color TeamColor;
+    
+    
 }
